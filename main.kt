ed8a6149df83ed6d3eaee7b022a8aef4610c9efe
“kkiirr201314” <qq255073@gmail.com>
2021-06-21 10:31:07 +0800

    重構healthStatus

--- main.kt
+++ main.kt
@@ -17,15 +17,24 @@ fun main() {
         else -> "無光環"
     }
 
-    val healthStatus = when (healthPoints ) {
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+    val statusFormatString = "(健康指數: $healthPoints)(光環: $auraColor) (運勢: ${if (isBlessed) "走運" else "很背"}) -> $name $healthStatus"
+
+    println(statusFormatString)
+}
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
+    val healthStatus = when (healthPoints) {
         100 -> "健康狀態極佳"
         in 90..99 -> "有一些小擦傷"
-        in 75..89 -> if (isBlessed) {"雖有一些傷口，但恢復很快"} else {"有一些傷口"}
+        in 75..89 -> if (isBlessed) {
+            "雖有一些傷口，但恢復很快"
+        } else {
+            "有一些傷口"
+        }
         in 15..74 -> "嚴重受傷"
         //顯示玩家狀態
         else -> "情況不妙"
     }
-    val statusFormatString = "(健康指數: $healthPoints)(光環: $auraColor) (運勢: ${if (isBlessed) "走運" else "很背"}) -> $name $healthStatus"
-
-    println(statusFormatString)
+    return healthStatus
 }
